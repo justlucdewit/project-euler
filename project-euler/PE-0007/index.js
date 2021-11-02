b52c96a29452283d352564cd581cf2cc3d55c324
luke <luc.dewit@hotmail.nl>
2021-11-02 17:50:49 +0100

    PE-0007

---
+++ project-euler/PE-0007/index.js
@@ -0,0 +1,21 @@
+const isPrime = n => {
+	for(let i = 2, s = Math.sqrt(n); i <= s; i++)
+        if(n % i === 0) return false;
+
+    return n > 1;
+};
+
+let n = 1;
+let primes_found = 0;
+
+while (true) {
+    if (isPrime(n))
+        ++primes_found;
+    
+    if (primes_found === 10001)
+        break;
+
+    n += 2; // We can skip 2 because a prime is never even
+}
+
+console.log(n)
